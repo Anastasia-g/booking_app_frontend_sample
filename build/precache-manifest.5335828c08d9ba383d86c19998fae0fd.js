ea18607aca1611fdbdd6984356c9232c2f9cf9c1
Anastasia-g <anastasiagrbz@gmail.com>
2020-06-10 19:14:13 +0200

    version 1.02

---
+++ build/precache-manifest.5335828c08d9ba383d86c19998fae0fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d349525e1d5ef6d74141ffe495a4b12a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af45d5361dcaa33ec3b9",
+    "url": "/static/css/main.fbded35f.chunk.css"
+  },
+  {
+    "revision": "ba6bcfe5b19dce1d602a",
+    "url": "/static/js/2.a8ab795a.chunk.js"
+  },
+  {
+    "revision": "15c33a81f958097d85fb38e5c966519d",
+    "url": "/static/js/2.a8ab795a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af45d5361dcaa33ec3b9",
+    "url": "/static/js/main.b91d8a2b.chunk.js"
+  },
+  {
+    "revision": "55fdaf4562df8a2f5605",
+    "url": "/static/js/runtime-main.4043c508.js"
+  }
+]);
